fb061ef2cfda5cd731810aeb28cb78a96e7697e8
benniaobuguai@gmail.com <benniaobuguai@gmail.com>
2016-05-14 15:35:09 +0800

    change package and improve sth

--- app/build/generated/source/r/debug/com/opencdk/R.java
+++ app/build/generated/source/r/debug/com/opencdk/R.java
@@ -11,6 +11,6 @@ public final class R {
 		public static final int ic_launcher = 0x7f02000a;
 	}
 	public static final class string {
-		public static final int app_name = 0x7f050001;
+		public static final int app_name = 0x7f060001;
 	}
 }
--- opencdk/src/main/java/com/opencdk/view/viewpager/AutoScrollableView.java
+++ opencdk/src/main/java/com/opencdk/view/viewpager/AutoScrollableView.java
@@ -1,18 +1,17 @@
 package com.opencdk.view.viewpager;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import android.content.Context;
 import android.os.Parcelable;
-import android.support.v4.view.PagerAdapter;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Auto scroll viewpager
- * 
+ *
  * @author 笨鸟不乖
  * @email benniaobuguai@gmail.com
  * @version 1.0.0
@@ -106,54 +105,36 @@ public abstract class AutoScrollableView<T> extends AutoScrollViewPager implemen
 		return getItemList().get(position % mItemList.size());
 	}
 
-	/* (non-Javadoc)
-	 * @see com.wo2b.sdk.view.viewpager.IPagerAdapter#destroyItem(android.view.ViewGroup, int, java.lang.Object)
-	 */
 	@Override
 	public void destroyItem(ViewGroup container, int position, Object object)
 	{
 		container.removeView((View) object);
 	}
 
-	/* (non-Javadoc)
-	 * @see com.wo2b.sdk.view.viewpager.IPagerAdapter#getCount()
-	 */
 	@Override
 	public int getCount()
 	{
 		return 0;
 	}
 
-//	/* (non-Javadoc)
-//	 * @see com.wo2b.sdk.view.viewpager.IPagerAdapter#instantiateItem(android.view.ViewGroup, int)
-//	 */
 //	@Override
 //	public Object instantiateItem(ViewGroup view, int position)
 //	{
 //		return null;
 //	}
 
-	/* (non-Javadoc)
-	 * @see com.wo2b.sdk.view.viewpager.IPagerAdapter#isViewFromObject(android.view.View, java.lang.Object)
-	 */
 	@Override
 	public boolean isViewFromObject(View view, Object object)
 	{
 		return view.equals(object);
 	}
 
-	/* (non-Javadoc)
-	 * @see com.wo2b.sdk.view.viewpager.IPagerAdapter#restoreState(android.os.Parcelable, java.lang.ClassLoader)
-	 */
 	@Override
 	public void restoreState(Parcelable state, ClassLoader loader)
 	{
 		
 	}
 
-	/* (non-Javadoc)
-	 * @see com.wo2b.sdk.view.viewpager.IPagerAdapter#saveState()
-	 */
 	@Override
 	public Parcelable saveState()
 	{
